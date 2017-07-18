de5e7118c3d60482d45a756016998fa5b91027dd
liuyuhui0111 <84964308@qq.com>
2017-07-18 15:12:40 +0800

    新增插件
    fastclick babelruntime babelpoliffy

---
+++ src/components/standard/Standard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="standard">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'standard',
+  data () {
+    return {
+      msg: 'standard标准产品'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
